cac9c4ada1aee2b67e56492ab8e8e8b0a6f52b87
Ujan Ganguli <113851171+UjanGanguli@users.noreply.github.com>
2026-04-01 02:50:46 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@
 ║   Author: Ujan | Built for NSE Equities                          ║
 ╚══════════════════════════════════════════════════════════════════╝
 """
-
+pip install scipy
 import streamlit as st
 import pandas as pd
 import numpy as np
